2e7361d008c4a007f80ac1f8aabd6113064d6dd8
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-12 15:05:12 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -5,5 +5,17 @@ from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
-
+Department.drop_table()
+Department.create_table()
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+hr = Department.create("Human Resources", "building C, East Wing")
+print(hr)
+hr.name = 'HR'
+hr.location = "Bullding F, 10th floor"
+hr.update()
+print(hr)
+print("Delete Payroll")
+payroll.delete()
+print(payroll)
 ipdb.set_trace()
